f548817fe8d3b6a54df931567028eb1f19255c17
Jason Reilly <jdr0887@gmail.com>
2022-04-04 13:25:20 -0400

    filling out implementation

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,14 +1,14 @@
 import datetime
 import logging
 import os
+import shutil
 import traceback
 import uuid
 from datetime import datetime
 from logging.config import dictConfig
-from typing import Optional
-import csv
-import docker
+
 import aiofiles
+import docker
 from docker.errors import ContainerError
 from fastapi import FastAPI, File, UploadFile, Depends, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
@@ -69,11 +69,29 @@ app.add_middleware(
 # curl -X 'GET'    'http://localhost:8083/openapi.json' -H 'accept: application/json' 2> /dev/null |python -m json.tool |jq '.paths."/submit".post.parameters[].name' 
 
 
+def get_results(task_path: str, file_name: str):
+    results_file = os.path.join(task_path, file_name)
+    logger.info(f"reading: {results_file}")
+    if not os.path.exists(results_file):
+        raise HTTPException(status_code=404, detail="Not found")
+    results_data = []
+    results_feature_count = 0
+    with open(results_file, 'r') as csvfile:
+        data = csvfile.readlines()
+        for line in data:
+            if results_feature_count == 0:
+                results_feature_count = len(line.split(','))
+            results_data.append(tuple(line.strip().split(',')))
+    dimension = [len(results_data), results_feature_count]
+    logger.info(f"dimension: {dimension}")
+    return dimension, results_data
+
+
 @app.post("/submit", description="Submit an analysis")
-async def analyze(submitter_id: str = Query(default=None, description="unique identifier for the submitter (e.g., email)"),
+async def analyze(submitter_id: str = Query(default=..., description="unique identifier for the submitter (e.g., email)"),
                   gene_expression_data: UploadFile = File(default=None, description="Gene Expression Data (csv)"),
-                  phenotype_data: Optional[bytes] = File(default=None, description="Phenotype Data (csv)"),
                   parameters: Parameters = Depends(Parameters.as_form)):
+    logger.debug(msg=f"submitter_id: {submitter_id}")
     try:
         start_time = datetime.now()
 
@@ -82,7 +100,7 @@ async def analyze(submitter_id: str = Query(default=None, description="unique id
 
         task_id = str(uuid.uuid4())[:8]
         task_path = os.path.abspath(f"/app/data/{task_id}")
-        os.makedirs(task_path, exist_ok=True)
+        os.makedirs(task_path, exist_ok=False)
 
         param_path = os.path.join(task_path, "parameters.json")
         with open(param_path, 'w', encoding='utf-8') as f:
@@ -94,57 +112,60 @@ async def analyze(submitter_id: str = Query(default=None, description="unique id
             content = await gene_expression_data.read()
             await out_file.write(content)
 
-        if phenotype_data is not None:
-            phenotype_data_file_path = os.path.join(task_path, "phenoDataMatrix.csv")
-            async with aiofiles.open(phenotype_data_file_path, 'wb') as out_file:
-                await out_file.write(phenotype_data)
-
         image = "hmasson/cellfie-standalone-app:v2"
         volumes = {
             'cellfie-data': {'bind': '/data', 'mode': 'rw'},
             'cellfie-input-data': {'bind': '/input', 'mode': 'rw'},
         }
-        command = f"/data/{task_id}/geneBySampleMatrix.csv {parameters.SampleNumber} {parameters.Ref} {parameters.ThreshType} {parameters.PercentileOrValue} {global_value} {parameters.LocalThresholdType} {local_values} /data"
+        command = f"/data/{task_id}/geneBySampleMatrix.csv {parameters.SampleNumber} {parameters.Ref} {parameters.ThreshType} {parameters.PercentileOrValue} {global_value} {parameters.LocalThresholdType} {local_values} /data/{task_id}"
         try:
-            cellfie_container_logs = client.containers.run(image, volumes=volumes, name=task_id, working_dir="/input", privileged=True, remove=True, command=command)
+            cellfie_container_logs = client.containers.run(image, volumes=volumes, name=task_id, working_dir="/input", privileged=True, remove=True, command=command, detach=False)
             cellfie_container_logs_decoded = cellfie_container_logs.decode("utf8")
             logger.info(cellfie_container_logs_decoded)
         except ContainerError as err:
             logger.exception(err)
 
         end_time = datetime.now()
-        duration = end_time-start_time
+        duration = end_time - start_time
         logger.debug(msg=f"run duration: {divmod(duration.seconds, 60)}")
 
-        detail_scoring_file = os.path.join(task_path, "detailScoring.csv")
-        if not os.path.exists(detail_scoring_file):
-            raise HTTPException(status_code=404, detail="Not found")
-        detail_scoring_data = []
-        feature_count = 0
-        with open(detail_scoring_file, 'r') as csvfile:
-            data = csvfile.readlines()
-            for line in data:
-                if feature_count == 0:
-                    feature_count = len(line.split(','))
-                detail_scoring_data.append(tuple(line.strip().split(',')))
+        (detail_scoring_dim, detail_scoring_data) = get_results(task_path=task_path, file_name="detailScoring.csv")
+        (score_binary_dim, score_binary_data) = get_results(task_path=task_path, file_name="score_binary.csv")
+        (score_dim, score_data) = get_results(task_path=task_path, file_name="score.csv")
+        (task_info_dim, task_info_data) = get_results(task_path=task_path, file_name="taskInfo.csv")
 
-# detailScoring.csv
-# score.csv
-
-        # return_object = AnalysisResults()
-        return_object = {}
-        return_object["submitter_id"] = submitter_id
-        return_object["start_time"] = start_time
-        return_object["end_time"] = end_time
-        return_object["contents"] = [
+        return_object = {"submitter_id": submitter_id, "start_time": start_time, "end_time": end_time, "results": [
+            {
+                "name": "Detail Scoring",
+                "results_type": "CellFIE",
+                "spec": "",
+                "dimension": detail_scoring_dim,
+                "data": detail_scoring_data
+            },
+            {
+                "name": "Score Binary",
+                "results_type": "CellFIE",
+                "spec": "",
+                "dimension": score_binary_dim,
+                "data": score_binary_data
+            },
+            {
+                "name": "Score",
+                "results_type": "CellFIE",
+                "spec": "",
+                "dimension": score_dim,
+                "data": score_data
+            },
             {
                 "name": "Detail Scoring Table",
                 "results_type": "CellFIE",
                 "spec": "",
-                "size": [len(detail_scoring_data), feature_count],
-                "detailScoring": detail_scoring_data
+                "dimension": task_info_dim,
+                "data": task_info_data
             }
-        ]
+        ]}
+
+        shutil.rmtree(task_path)
 
         return return_object
 
