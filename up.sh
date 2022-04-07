b988f4771fddbb068be3720eb714f76ee9412c09
Jason Reilly <jdr0887@gmail.com>
2022-04-07 09:20:07 -0400

    creating fuse network

--- up.sh
+++ up.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-export $(cat .env|grep -v '^#')
+set -a
+. .env
+set +a
 
-docker-compose -f docker-compose.yml up --build -V --remove-orphans -d
+CMD=`docker network ls | awk '{print $2}' | grep -w fuse | head -1`
+RET=$CMD
+if [ "$RET" == "fuse" ]; then
+    echo "found fuse, joining network";
+else
+    echo "creating and joining fuse network";
+    docker network create -d bridge fuse
+fi
+docker-compose -f docker-compose.yml up --build -V -d
