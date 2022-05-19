5a77270bd346ee08877535e6ff5e990a2478070e
Jason Reilly <jdr0887@gmail.com>
2022-05-19 13:21:56 -0400

    fixing imports

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -16,7 +16,7 @@ from docker.errors import ContainerError
 from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-from fuse_utilities.main import ToolParameters
+from fuse_cdm.main import ToolParameters
 
 LOGGING = {
     'version': 1,
